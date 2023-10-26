a1092e9968c0aa43a97e3f86a72a54604e31253c
Sabbir Bhai 36 <138810861+Rdx337@users.noreply.github.com>
2023-10-26 23:58:12 +0600

    Update SR.py

--- SR.py
+++ SR.py
@@ -1,25 +1,34 @@
-#MD SABBIR BHAI
-#FACBOOK MD SABBIR SR
-#--------------import-----#
-import os 
-#--------------logo---------#
-logo(
-  
- __  __     _    _____       _     _     _         _____      
- |  \/  |   | |  / ____|     | |   | |   (_)       / ____|     
- | \  / | __| | | (___   __ _| |__ | |__  _ _ __  | (___  _ __ 
- | |\/| |/ _` |  \___ \ / _` | '_ \| '_ \| | '__|  \___ \| '__|
- | |  | | (_| |  ____) | (_| | |_) | |_) | | |     ____) | |   
- |_|  |_|\__,_| |_____/ \__,_|_.__/|_.__/|_|_|    |_____/|_|                                                                                                                                                                                         
-#--------------clear------------#
-def clers():
-       os.systeam('clear')
-        print (logo)
-        print(42*'-')
-        print (FB ID : MD Sabbir SR
-        print( GITHUB: RDX337
-        
-#---------------menu---------#
-#--------------end-------------#
+#SABBIR BHAI 
+#MY WASTAP +8801840762780
+#-----------------import-----------------#
+#----------------logo----------------------#
+logo=("
+                                                                                                                                                                                                                                                  dddddddd                                         bbbbbbbb           bbbbbbbb                                                                                  
+MMMMMMMM               MMMMMMMM            d::::::d        SSSSSSSSSSSSSSS                  b::::::b           b::::::b              iiii                             SSSSSSSSSSSSSSS                    
+M:::::::M             M:::::::M            d::::::d      SS:::::::::::::::S                 b::::::b           b::::::b             i::::i                          SS:::::::::::::::S                   
+M::::::::M           M::::::::M            d::::::d     S:::::SSSSSS::::::S                 b::::::b           b::::::b              iiii                          S:::::SSSSSS::::::S                   
+M:::::::::M         M:::::::::M            d:::::d      S:::::S     SSSSSSS                  b:::::b            b:::::b                                            S:::::S     SSSSSSS                   
+M::::::::::M       M::::::::::M    ddddddddd:::::d      S:::::S              aaaaaaaaaaaaa   b:::::bbbbbbbbb    b:::::bbbbbbbbb    iiiiiiirrrrr   rrrrrrrrr        S:::::S           rrrrr   rrrrrrrrr   
+M:::::::::::M     M:::::::::::M  dd::::::::::::::d      S:::::S              a::::::::::::a  b::::::::::::::bb  b::::::::::::::bb  i:::::ir::::rrr:::::::::r       S:::::S           r::::rrr:::::::::r  
+M:::::::M::::M   M::::M:::::::M d::::::::::::::::d       S::::SSSS           aaaaaaaaa:::::a b::::::::::::::::b b::::::::::::::::b  i::::ir:::::::::::::::::r       S::::SSSS        r:::::::::::::::::r 
+M::::::M M::::M M::::M M::::::Md:::::::ddddd:::::d        SS::::::SSSSS               a::::a b:::::bbbbb:::::::bb:::::bbbbb:::::::b i::::irr::::::rrrrr::::::r       SS::::::SSSSS   rr::::::rrrrr::::::r
+M::::::M  M::::M::::M  M::::::Md::::::d    d:::::d          SSS::::::::SS      aaaaaaa:::::a b:::::b    b::::::bb:::::b    b::::::b i::::i r:::::r     r:::::r         SSS::::::::SS  r:::::r     r:::::r
+M::::::M   M:::::::M   M::::::Md:::::d     d:::::d             SSSSSS::::S   aa::::::::::::a b:::::b     b:::::bb:::::b     b:::::b i::::i r:::::r     rrrrrrr            SSSSSS::::S r:::::r     rrrrrrr
+M::::::M    M:::::M    M::::::Md:::::d     d:::::d                  S:::::S a::::aaaa::::::a b:::::b     b:::::bb:::::b     b:::::b i::::i r:::::r                             S:::::Sr:::::r            
+M::::::M     MMMMM     M::::::Md:::::d     d:::::d                  S:::::Sa::::a    a:::::a b:::::b     b:::::bb:::::b     b:::::b i::::i r:::::r                             S:::::Sr:::::r            
+M::::::M               M::::::Md::::::ddddd::::::dd     SSSSSSS     S:::::Sa::::a    a:::::a b:::::bbbbbb::::::bb:::::bbbbbb::::::bi::::::ir:::::r                 SSSSSSS     S:::::Sr:::::r            
+M::::::M               M::::::M d:::::::::::::::::d     S::::::SSSSSS:::::Sa:::::aaaa::::::a b::::::::::::::::b b::::::::::::::::b i::::::ir:::::r                 S::::::SSSSSS:::::Sr:::::r            
+M::::::M               M::::::M  d:::::::::ddd::::d     S:::::::::::::::SS  a::::::::::aa:::ab:::::::::::::::b  b:::::::::::::::b  i::::::ir:::::r                 S:::::::::::::::SS r:::::r            
+MMMMMMMM               MMMMMMMM   ddddddddd   ddddd      SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaabbbbbbbbbbbbbbbb   bbbbbbbbbbbbbbbb   iiiiiiiirrrrrrr                  SSSSSSSSSSSSSSS   rrrrrrr ")
+#----------------clear-------------------#
+def clear ():
+     os.systeam(`clear`)
+print(logo)
+pint(42*'-')
+print(`FB ID : MD SABBIR SR
+print(`GITHUB : RDX337
+print(` ADMIN: SABBIR                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+#---------------menu--------------------#
 
-clear()
+#--------------end------------------------#
+clear():
