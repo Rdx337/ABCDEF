7ba627b357c8662428f7ac9d045780a4a926597d
Sabbir Bhai 36 <138810861+Rdx337@users.noreply.github.com>
2023-10-27 00:01:58 +0600

    Update SR.py

--- SR.py
+++ SR.py
@@ -2,7 +2,7 @@
 #MY WASTAP +8801840762780
 #-----------------import-----------------#
 #----------------logo----------------------#
-logo=("
+logo=('''
                                                                                                                                                                                                                                                   dddddddd                                         bbbbbbbb           bbbbbbbb                                                                                  
 MMMMMMMM               MMMMMMMM            d::::::d        SSSSSSSSSSSSSSS                  b::::::b           b::::::b              iiii                             SSSSSSSSSSSSSSS                    
 M:::::::M             M:::::::M            d::::::d      SS:::::::::::::::S                 b::::::b           b::::::b             i::::i                          SS:::::::::::::::S                   
@@ -19,7 +19,7 @@ M::::::M     MMMMM     M::::::Md:::::d     d:::::d                  S:::::Sa::::
 M::::::M               M::::::Md::::::ddddd::::::dd     SSSSSSS     S:::::Sa::::a    a:::::a b:::::bbbbbb::::::bb:::::bbbbbb::::::bi::::::ir:::::r                 SSSSSSS     S:::::Sr:::::r            
 M::::::M               M::::::M d:::::::::::::::::d     S::::::SSSSSS:::::Sa:::::aaaa::::::a b::::::::::::::::b b::::::::::::::::b i::::::ir:::::r                 S::::::SSSSSS:::::Sr:::::r            
 M::::::M               M::::::M  d:::::::::ddd::::d     S:::::::::::::::SS  a::::::::::aa:::ab:::::::::::::::b  b:::::::::::::::b  i::::::ir:::::r                 S:::::::::::::::SS r:::::r            
-MMMMMMMM               MMMMMMMM   ddddddddd   ddddd      SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaabbbbbbbbbbbbbbbb   bbbbbbbbbbbbbbbb   iiiiiiiirrrrrrr                  SSSSSSSSSSSSSSS   rrrrrrr ")
+MMMMMMMM               MMMMMMMM   ddddddddd   ddddd      SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaabbbbbbbbbbbbbbbb   bbbbbbbbbbbbbbbb   iiiiiiiirrrrrrr                  SSSSSSSSSSSSSSS   rrrrrrr ''')
 #----------------clear-------------------#
 def clear ():
      os.systeam(`clear`)
