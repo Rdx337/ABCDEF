00880e73f91e70127eb541f4c380ae58255b0b58
Sabbir Bhai 36 <138810861+Rdx337@users.noreply.github.com>
2023-10-27 01:11:01 +0600

    Update SM.py

--- SM.py
+++ SM.py
@@ -1,27 +1,28 @@
-#SABBIR BHAI 
-#MY WASTAP +8801840762780
-#-----------------import-----------------#
-import os
-#----------------logo----------------------#
-logo=('''                                                                                                                                                                                                                                                                                                  
+ # HI ALL FRENDE WELCOME TO MY COMMAND AND ENJOY MY COMAND
+#FOLLOW MY FACEBOOK ID https://www.facebook.com/profile.php?id=100089747571096&mibextid=ZbWKwL
+#JOINE MY FACEBOOK ID 
 
+logo=('''
 ███████╗ █████╗ ██████╗ ██████╗ ██╗██████╗ 
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗
 ███████╗███████║██████╔╝██████╔╝██║██████╔╝
 ╚════██║██╔══██║██╔══██╗██╔══██╗██║██╔══██╗
 ███████║██║  ██║██████╔╝██████╔╝██║██║  ██║
-╚══════╝╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═╝    ''')
-#----------------clear-------------------#
+╚══════╝╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═╝ ''')
+
 def clear():
-	os. system('clear')
+c('clear')
 print(logo)
+print(40*'-')
 print('FB ID : MD SABBIR SR')
 print('GITHUB : RDX337')
-print('ADMIN: SABBIR ')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-#---------------menu--------------------#
+pring(' WHATSAPP : +8801840762780')
+print(40*'-')
 def main():
-	print('[A]FILE CLONING')
-	print('[B]Ext')
-	option=inport('[?] CHOICE MENU :')
-     
-#--------------end------------------------#
+ clear()
+  print('[A]  FILE CLONING')
+  print('[B]EXIT')
+  
+  
+                                          
+                                           
