36ec6b185ff6974f867b037f7c0535dbe05aa6de
Sabbir Bhai 36 <138810861+Rdx337@users.noreply.github.com>
2023-10-27 00:11:28 +0600

    Add files via upload

---
+++ SM.py
@@ -0,0 +1,37 @@
+#SABBIR BHAI 
+#MY WASTAP +8801840762780
+#-----------------import-----------------#
+#----------------logo----------------------#
+logo=('''
+                                                                                                                                                                                                                                                  dddddddd                                         bbbbbbbb           bbbbbbbb                                                                                  
+MMMMMMMM               MMMMMMMM            d::::::d        SSSSSSSSSSSSSSS                  b::::::b           b::::::b              iiii                             SSSSSSSSSSSSSSS                    
+M:::::::M             M:::::::M            d::::::d      SS:::::::::::::::S                 b::::::b           b::::::b             i::::i                          SS:::::::::::::::S                   
+M::::::::M           M::::::::M            d::::::d     S:::::SSSSSS::::::S                 b::::::b           b::::::b              iiii                          S:::::SSSSSS::::::S                   
+M:::::::::M         M:::::::::M            d:::::d      S:::::S     SSSSSSS                  b:::::b            b:::::b                                            S:::::S     SSSSSSS                   
+M::::::::::M       M::::::::::M    ddddddddd:::::d      S:::::S              aaaaaaaaaaaaa   b:::::bbbbbbbbb    b:::::bbbbbbbbb    iiiiiiirrrrr   rrrrrrrrr        S:::::S           rrrrr   rrrrrrrrr   
+M:::::::::::M     M:::::::::::M  dd::::::::::::::d      S:::::S              a::::::::::::a  b::::::::::::::bb  b::::::::::::::bb  i:::::ir::::rrr:::::::::r       S:::::S           r::::rrr:::::::::r  
+M:::::::M::::M   M::::M:::::::M d::::::::::::::::d       S::::SSSS           aaaaaaaaa:::::a b::::::::::::::::b b::::::::::::::::b  i::::ir:::::::::::::::::r       S::::SSSS        r:::::::::::::::::r 
+M::::::M M::::M M::::M M::::::Md:::::::ddddd:::::d        SS::::::SSSSS               a::::a b:::::bbbbb:::::::bb:::::bbbbb:::::::b i::::irr::::::rrrrr::::::r       SS::::::SSSSS   rr::::::rrrrr::::::r
+M::::::M  M::::M::::M  M::::::Md::::::d    d:::::d          SSS::::::::SS      aaaaaaa:::::a b:::::b    b::::::bb:::::b    b::::::b i::::i r:::::r     r:::::r         SSS::::::::SS  r:::::r     r:::::r
+M::::::M   M:::::::M   M::::::Md:::::d     d:::::d             SSSSSS::::S   aa::::::::::::a b:::::b     b:::::bb:::::b     b:::::b i::::i r:::::r     rrrrrrr            SSSSSS::::S r:::::r     rrrrrrr
+M::::::M    M:::::M    M::::::Md:::::d     d:::::d                  S:::::S a::::aaaa::::::a b:::::b     b:::::bb:::::b     b:::::b i::::i r:::::r                             S:::::Sr:::::r            
+M::::::M     MMMMM     M::::::Md:::::d     d:::::d                  S:::::Sa::::a    a:::::a b:::::b     b:::::bb:::::b     b:::::b i::::i r:::::r                             S:::::Sr:::::r            
+M::::::M               M::::::Md::::::ddddd::::::dd     SSSSSSS     S:::::Sa::::a    a:::::a b:::::bbbbbb::::::bb:::::bbbbbb::::::bi::::::ir:::::r                 SSSSSSS     S:::::Sr:::::r            
+M::::::M               M::::::M d:::::::::::::::::d     S::::::SSSSSS:::::Sa:::::aaaa::::::a b::::::::::::::::b b::::::::::::::::b i::::::ir:::::r                 S::::::SSSSSS:::::Sr:::::r            
+M::::::M               M::::::M  d:::::::::ddd::::d     S:::::::::::::::SS  a::::::::::aa:::ab:::::::::::::::b  b:::::::::::::::b  i::::::ir:::::r                 S:::::::::::::::SS r:::::r            
+MMMMMMMM               MMMMMMMM   ddddddddd   ddddd      SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaabbbbbbbbbbbbbbbb   bbbbbbbbbbbbbbbb   iiiiiiiirrrrrrr                  SSSSSSSSSSSSSSS   rrrrrrr ''')
+#----------------clear-------------------#
+def clear ():
+	
+     os.system('clear`)
+     
+print(logo)
+pint(42*'-')
+print(`FB ID : MD SABBIR SR
+print(`GITHUB : RDX337
+print(` ADMIN: SABBIR                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+#---------------menu--------------------#
+
+
+#--------------end------------------------#
+clear():
