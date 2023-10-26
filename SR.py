426923ae17416780e25f8dbc6d441dae082b49ec
Sabbir Bhai 36 <138810861+Rdx337@users.noreply.github.com>
2023-10-27 00:04:27 +0600

    Update SR.py

--- SR.py
+++ SR.py
@@ -22,7 +22,7 @@ M::::::M               M::::::M  d:::::::::ddd::::d     S:::::::::::::::SS  a:::
 MMMMMMMM               MMMMMMMM   ddddddddd   ddddd      SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaabbbbbbbbbbbbbbbb   bbbbbbbbbbbbbbbb   iiiiiiiirrrrrrr                  SSSSSSSSSSSSSSS   rrrrrrr ''')
 #----------------clear-------------------#
 def clear ():
-     os.systeam(`clear`)
+     os.system(`clear`)
 print(logo)
 pint(42*'-')
 print(`FB ID : MD SABBIR SR
